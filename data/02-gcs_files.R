ef919711051c2e6fa8733f7d65872d6e04d4aa7c
Misha Berrien <misha.berrien@gmail.com>
2020-02-07 12:14:21 -0600

    Can read in data through projecttemplate framework

--- data/02-gcs_files.R
+++ data/02-gcs_files.R
@@ -2,8 +2,32 @@
 # Location: raw_data_storage/ ISBE_Student_Courses/
 
 library(googleCloudStorageR)
-
+library(tidyverse)
+library(janitor)
 gcs_global_bucket("raw_data_storage")
+
+# pull middle school grades
+all_grade_files <- gcs_list_objects()
+middle_school_18_18_grade_files <- all_grade_files$name[6:36]
+
+count = 1
+for (file_name in middle_school_18_18_grade_files) {
+  gcs_get_object(file_name,
+                 saveToDisk = paste("data/flatfiles/Middle school 4-8/SY18_19_", count, ".csv", sep = ""), 
+                 overwrite = TRUE)
+  count = count + 1
+}
+
+# pull primary grades
+primary_school_17_18_grade_files <- all_grade_files$name[37:51]
+
+count = 1
+for (file_name in primary_school_17_18_grade_files) {
+  gcs_get_object(file_name,
+                 saveToDisk = paste("data/flatfiles/Primary school K-3/SY17_18_", count, ".csv", sep = ""), 
+                 overwrite = TRUE)
+  count = count + 1
+}
 
 gcs_get_object("ISBE_Student_Courses/18-18_files/190628_Days_Absent.csv", 
                saveToDisk = "data/flatfiles/190628_Days_Absent.csv", 
@@ -21,7 +45,8 @@ gcs_get_object("ISBE_Student_Courses/18-18_files/isbe_report_courses_2017.csv",
                saveToDisk = "data/flatfiles/isbe_report_courses_2017.csv", 
                overwrite = TRUE)
 
-# Read in Files -----------------------------------------------------------
+
+# Read in Files to Environment -----------------------------------------------------------
 teach_absent <-
   read_csv(here::here("data", "flatfiles", "190628_Days_Absent.csv")) %>%
   janitor::clean_names() %>%
@@ -69,16 +94,13 @@ missing_iein_dob <-
 
 # old submission from 2017. this doesn't have all the right courses so she pulls
 # another ISBE Report on top of it. 
-
 isbe_report_2017 <- 
   read_csv(here::here("data", "flatfiles", "isbe_report_courses_2017.csv")) %>%
   as_tibble() %>%
   janitor::clean_names()
 
-----
-# Grades (from report card)
 file_list_middle <- 
-  dir(path = here::here("data", "flatfiles", "flatfiles", "Middle school 4-8/"), 
+  dir(path = here::here("data", "flatfiles", "Middle school 4-8/"), 
       pattern = "SY18_19", full.names = TRUE)
 
 grade_df_list_middle <- 
@@ -86,15 +108,12 @@ grade_df_list_middle <-
   map(read_csv) %>%
   map(clean_names)
 
-gcs_get_object("ISBE_Student_Courses/18-18_files/IEINs & DOBs.xlsx",
-               saveToDisk = "data/flatfiles/IEINs & DOBs.csv")
-
-## pull grades for primary
 file_list_primary <- 
-  dir(path = here::here("data", "Primary school K-3/"), 
-      pattern = "SY18_19", full.names = TRUE)
+  dir(path = here::here("data", "flatfiles", "Primary school K-3/"), 
+      pattern = "SY17_18", full.names = TRUE)
 
 grade_df_list_prim <- 
   file_list_primary %>%
   map(read_csv) %>%
   map(clean_names)
+
