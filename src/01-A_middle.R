aff50253a7b3c8838d20998231e66adda49f1dd2
carolinekinnen <caroline.kinnen77@gmail.com>
2020-02-11 16:27:28 -0600

    combined state course ID file with primary and middle files

--- src/01-A_middle.R
+++ src/01-A_middle.R
@@ -6,7 +6,8 @@ source(here::here("munge", "01-A.R"))
 
 # Student Courses --------------------------------------------------
 
-# add in state course id
+# State course IDs come from flat file and is joined with existing local course IDs from Powerschool enrollment
+# Multiple courses per student
 
 students_course_middle <- students_local_course_id_title_section_number %>%
   left_join(local_number_isbe_state_course_ids, by = "local_course_id") %>%
--- src/01-A_primary.R
+++ src/01-A_primary.R
@@ -6,11 +6,36 @@ source(here::here("munge", "01-A.R"))
 
 # Student Courses --------------------------------------------------
 
-# add in state course id
+# Subjects, state course IDs, and local course IDs (made up because not enrolled in Powerschool) come from a flat file
+# Multiple core courses per student
 
-students_course_primary <- students_local_course_id_title_section_number %>%
-  filter(grepl("k|1|2|3", local_course_title)) %>%
-  mutate(school = 
-           school= "extract school"  #str_extract(school, "[1-3]|k"),
-         grade_level = "extract number") %>%
-  left_join(local_number_isbe_state_course_ids, by = c("subject", "grade_level", "school"))
+students_course_primary_core <- students_local_course_id_title_section_number %>%
+  filter(grepl("k |1|2|3", local_course_title),
+         !grepl("Science", local_course_title)) %>%
+  mutate(school = str_extract(local_course_id, "kop|kbp|kap|kacp"),
+         grade_level = str_replace(local_course_id, "kop|kbp|kap|kacp", "") %>%       
+           str_extract("^.")) %>%
+  select(student_id, teacherid, school, grade_level) %>%
+  left_join(local_number_isbe_state_course_ids, by = c("grade_level", "school")) %>% 
+  filter(is.na(first_last_teacher)) %>%
+  select(-first_last_teacher)
+  
+
+# Subjects, state course IDs, local course IDs (made up because not enrolled in Powerschool) and teachers come from flat file
+# Multiple excellence courses per student
+
+students_course_primary_excellence <- students_local_course_id_title_section_number %>%
+  filter(grepl("k |1|2|3", local_course_title),
+         !grepl("Science", local_course_title)) %>%
+  mutate(school = str_extract(local_course_id, "kop|kbp|kap|kacp"),
+         grade_level = str_replace(local_course_id, "kop|kbp|kap|kacp", "") %>%      
+           str_extract("^.")) %>%
+  select(student_id, teacherid, school, grade_level) %>%
+  left_join(local_number_isbe_state_course_ids, by = c("grade_level", "school")) %>% 
+  filter(!is.na(first_last_teacher)) %>%
+  select(-teacherid)
+  
+  
+  
+  
+  
