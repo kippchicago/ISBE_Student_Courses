845316063f09597fa0c58761cbd2830ca5e02220
Misha Berrien <misha.berrien@gmail.com>
2020-02-25 06:43:51 -0600

    completed initial fix for students that appear in the incorrect report

--- src/01-A_produce-submission-files.R
+++ src/01-A_produce-submission-files.R
@@ -7,56 +7,73 @@ source(here::here("munge", "01-A.R"))
 source(here::here("munge", "02-A_primary.R"))
 source(here::here("munge", "03-A_middle.R"))
 
-# Individual Primary School Reports ---------------------------------------
 
-isbe_midyear_report_kap <- 
-  isbe_report_primary_midyear_2020_full %>%
-  filter(grepl("kap", `Local Course ID`))
+# Combine Middle and Primary Reports --------------------------------------
 
-isbe_midyear_report_kbp <- 
-  isbe_report_primary_midyear_2020_full %>%
-  filter(grepl("kbp", `Local Course ID`))
+isbe_report_all_schools <- 
+  bind_rows(isbe_report_middle_midyear_2020_full, 
+            isbe_report_primary_midyear_2020_full) %>%
+  drop_na(`Student First Name`)
 
-isbe_midyear_report_kacp <- 
-  isbe_report_primary_midyear_2020_full %>%
-  filter(grepl("kacp", `Local Course ID`))
+# Filter Report for all 4 Schools -----------------------------------------
 
-isbe_midyear_report_kop <- 
-  isbe_report_primary_midyear_2020_full %>%
-  filter(grepl("kop", `Local Course ID`))
+isbe_midyear_report_400044 <- 
+  isbe_report_all_schools %>%
+  filter(`CPS School ID` == 400044)
 
-# Individual Middle School Reports ----------------------------------------
+isbe_midyear_report_400146 <- 
+  isbe_report_all_schools %>%
+  filter(`CPS School ID` == 400146)
 
-isbe_midyear_report_kams <- 
-  isbe_report_middle_midyear_2020_full %>%
-  filter(grepl("kams", `Local Course ID`))
+isbe_midyear_report_400163 <- 
+  isbe_report_all_schools %>%
+  filter(`CPS School ID` == 400163)
 
-isbe_midyear_report_kbcp <- 
-  isbe_report_middle_midyear_2020_full %>%
-  filter(grepl("kbcp", `Local Course ID`))
+isbe_midyear_report_400180 <- 
+  isbe_report_all_schools %>%
+  filter(`CPS School ID` == 400180)
 
-isbe_midyear_report_kac <- 
-  isbe_report_middle_midyear_2020_full %>%
-  filter(grepl("kac", `Local Course ID`))
+# Student Roster by School -----------------------------------------
 
-isbe_midyear_report_koa <- 
-  isbe_report_middle_midyear_2020_full %>%
-  filter(grepl("koa", `Local Course ID`))
+ps_school_student_roster_400044 <- students_current_demographics %>%
+  select(cps_school_id, cps_student_id) %>%
+  distinct() %>%
+  filter(cps_school_id == 400044)
 
-# Final Combined Reports (4 Schools) -------------------------------------
+ps_school_student_roster_400146 <- students_current_demographics %>%
+  select(cps_school_id, cps_student_id) %>%
+  distinct() %>%
+  filter(cps_school_id == 400146)
 
-isbe_midyear_report_ascend <- 
-  bind_rows(isbe_midyear_report_kap, 
-            isbe_midyear_report_kams)
+ps_school_student_roster_400163 <- students_current_demographics %>%
+  select(cps_school_id, cps_student_id) %>%
+  distinct() %>%
+  filter(cps_school_id == 400163)
 
-isbe_midyear_report_bloom <- 
-  bind_rows(isbe_midyear_report_kbcp, 
-            isbe_midyear_report_kbp)
+ps_school_student_roster_400180 <- students_current_demographics %>%
+  select(cps_school_id, cps_student_id) %>%
+  distinct() %>%
+  filter(cps_school_id == 400180)
 
-isbe_midyear_report_one <- 
-  bind_rows(isbe_midyear_report_koa, 
-            isbe_midyear_report_kop)
 
-isbe_midyear_report_academy <- 
-  bind_rows(isbe_midyear_report_kac, 
-            isbe_midyear_report_kacp)
+# Rostered to the school but not in the report ------------------------------------
+
+missing_students_400044 <- 
+  ps_school_student_roster_400044 %>%
+  anti_join(isbe_midyear_report_400044, 
+            by = c("cps_student_id" = "CPS Student ID"))
+
+missing_students_400146 <- 
+  ps_school_student_roster_400146 %>%
+  anti_join(isbe_midyear_report_400146, 
+            by = c("cps_student_id" = "CPS Student ID"))
+
+missing_students_400163 <- 
+  ps_school_student_roster_400163 %>%
+  anti_join(isbe_midyear_report_400163, 
+            by = c("cps_student_id" = "CPS Student ID"))
+
+missing_students_400180 <- 
+  ps_school_student_roster_400180 %>%
+  anti_join(isbe_midyear_report_400180, 
+            by = c("cps_student_id" = "CPS Student ID"))
